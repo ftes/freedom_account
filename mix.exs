8efa615b271f7a367674a540850dda5d8423160a
Fredrik Teschke <f@ftes.de>
2026-09-08 18:33:39 +0200

    Use released Fluffy 0.1.0

--- mix.exs
+++ mix.exs
@@ -81,7 +81,7 @@ defmodule FreedomAccount.MixProject do
       {:bandit, "~> 1.10"},
       {:boundary, "~> 0.10.4"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:fluffy, path: System.get_env("FLUFFY_PATH", "../fluffy"), only: :test},
+      {:fluffy, "~> 0.1.0", only: :test},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:dns_cluster, "~> 0.2.0"},
       {:ecto_sql, "~> 3.13"},
